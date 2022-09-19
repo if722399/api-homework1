193db5fa31a8e1f7d2a7b936c954f68b04e0bdc3
Unknown <if722399@iteso.mx>
2022-09-19 11:38:06 -0500

    Creación del primer endpoint para la creación de usuarios

---
+++ main.py
@@ -0,0 +1,38 @@
+from time import time
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Union
+
+app = FastAPI()
+
+class Users(BaseModel):
+
+    user_name:str
+    user_id: int
+    user_email:str
+    age: Union[int,None]=None
+    recommendations: list[str]
+    zip: Union[int,None]=None
+
+
+user_dict = {}
+
+@app.put('/users')
+def create_post(user: Users):
+    user = user.dict()
+
+    if user['user_id'] in user_dict:
+        raise ValueError(f'User {user["user_id"]} already exists')
+
+    user_dict[user['user_id']] = user
+
+    return {'Description':f'User creado correctamente {user["user_id"]}, su nombre es: {user["user_name"]}'}
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info", reload=True)
+
